40c1552263431944ba50966ef054cc20e5ea9d11
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2026-05-06 19:36:09 +0200

    chore: add `bumpify` for semantic version bumps

--- tasks.py
+++ tasks.py
@@ -9,23 +9,41 @@ def format(ctx: Context):
 
 
 @task
-def coverage(ctx: Context):
-    """Check code coverage."""
-    ctx.run("pytest --cov-branch --cov=flatfs")
+def build(ctx: Context):
+    """Build package for deployment."""
+    ctx.run("poetry build")
 
 
 @task
-def lint(ctx: Context):
+def bump(ctx: Context):
+    """Bump project version."""
+    ctx.run("bumpify bump")
+
+
+@task
+def check_coverage(ctx: Context):
+    """Check code coverage."""
+    ctx.run("pytest --cov-branch --cov=flatfs --cov-fail-under=95")
+
+
+@task
+def check_formatting(ctx: Context):
+    """Check if the code is formatted."""
+    ctx.run("ruff format --line-length=120 --check")
+
+
+@task
+def check_linter(ctx: Context):
     """Run static code analyzer."""
     ctx.run("ruff check --exclude api.py")
 
 
 @task
-def test(ctx: Context):
+def check_tests(ctx: Context):
     """Run all tests."""
     ctx.run("pytest")
 
 
-@task(lint, test)
-def check(ctx: Context):
+@task(check_formatting, check_linter, check_coverage, check_tests)
+def check_all(ctx: Context):
     """Run all checks."""
